00923f6cf85bcd643cf8235b65a93f476681cd04
qapquiz <arm.armariya@gmail.com>
2023-09-27 20:55:59 +0700

    feat: add nodeResolve and commonjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,22 @@
-import typescript from '@rollup/plugin-typescript';
-import FastGlob from 'fast-glob';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import FastGlob from "fast-glob";
 
 export default {
-  input: FastGlob.globSync(['src/scenarios/*.ts']),
+  input: FastGlob.globSync(["src/scenarios/*.ts"]),
   output: {
     format: "es",
     dir: "dist",
     preserveModules: true,
-    preserveModulesRoot: 'src',
+    preserveModulesRoot: "src",
   },
   external: [new RegExp(/^(k6|https?\:\/\/)(\/.*)?/)],
-  plugins: [typescript({
-    tsconfig: false, // disable read config from tsconfig.json
-  })],
+  plugins: [
+    typescript({
+      tsconfig: false, // disable read config from tsconfig.toJSON();
+    }),
+    nodeResolve(),
+    commonjs(),
+  ],
 };
